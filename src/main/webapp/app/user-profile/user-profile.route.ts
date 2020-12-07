6ced4902d099129432601a56ea2d0d57c2013ff1
Saato97 <tomek111541@wp.pl>
2020-12-07 22:00:10 +0100

    Czyszczenie plików i poprawa navbaru

--- src/main/webapp/app/user-profile/user-profile.route.ts
+++ src/main/webapp/app/user-profile/user-profile.route.ts
@@ -1,13 +1,14 @@
 import { Route } from '@angular/router';
 
 import { UserRouteAccessService } from 'app/core/auth/user-route-access-service';
+import { Authority } from 'app/shared/constants/authority.constants';
 import { UserProfileComponent } from './user-profile.component';
 
 export const USER_PROFILE_ROUTE: Route = {
   path: 'user-profile',
   component: UserProfileComponent,
   data: {
-    authorities: [],
+    authorities: [Authority.USER],
     pageTitle: 'user-profile.title',
   },
   canActivate: [UserRouteAccessService],
